3b6a8ba9d08859c01633402085bf8d2cf7e394a2
jisaa_yoga <giuseppe.richetti@gmail.com>
2025-03-11 16:33:44 +0100

    add chatServer type

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.23.4
 require (
 	github.com/coder/websocket v1.8.12 // indirect
 	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
 )
--- src/chat/chat.go
+++ src/chat/chat.go
@@ -1,11 +1,56 @@
-package conn
+package chat
 
 import (
+	"log"
 	"net/http"
+	"sync"
 
-	//"github.com/coder/websocket"
+	"golang.org/x/time/rate"
 )
 
-func handleWebSocket(w http.ResponseWriter, r *http.Request){
+//"net/http"
 
+//"github.com/coder/websocket"
+
+// chatServer enables broadcasting to a range of subscribers.
+type chatServer struct {
+
+
+    // subscriberMessageBuffer holds the maximum messages per subscriber that can be queued
+    // before it is kicked out.
+    //
+    // Defaults to 16.
+    subscriberMessageBuffer int 
+
+    // publishLimiter limits the publishing to the endpoint.
+    //
+    // Defaults to one publish every 100ms with a burst of 8.
+    publishLimiter *rate.Limiter
+
+    // logf controls where logs are sent.
+    // It mirrors log.Printf, log.Fatalf functions arguments,
+    // e.g. When creating a new chatServer:
+    // cs := chatServer {
+    //          ...
+    //          logf: log.Fatalf,
+    //          ...
+    //       }
+    //
+    // Defaults to log.Printf.
+    logf func(f string, v ...any)
+
+    // serveMux routes the various endpoints to the appropiate handler.
+    //
+    // ServeMux is an HTTP request multiplexer.
+    // It matches the URL of each incoming request against a list of registered
+    // patterns and calls the handler for the pattern that
+    // most closely matches the URL.
+    // In general, a pattern looks like
+    //
+    //	[METHOD ][HOST]/[PATH]
+    serveMux    http.ServeMux
+
+    subscribersMu sync.Mutex
+    subscribers map[*subscriber]struct{}
 }
+
